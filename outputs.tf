648b96ee0dacbd947bf20e8d838312c359ad5959
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 18:39:27 -0500

    Add descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "bucket_arn" {
-  value = aws_s3_bucket.this[0].arn
+  value       = aws_s3_bucket.this[0].arn
+  description = "Bucket ARN(s)"
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.this[0].id
+  value       = aws_s3_bucket.this[0].id
+  description = "Bucket name(s)"
 }
