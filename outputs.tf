2aa44d1981e2e428290119189ba22ad726973311
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 23:05:00 -0500

    v1.0.1

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
-  value = ["${aws_s3_bucket.s3_bucket.*.arn}"]
+  value = ["${aws_s3_bucket.this.*.arn}"]
   description = "Bucket ARN(s)"
 }
 
 output "bucket_name" {
-  value = ["${aws_s3_bucket.s3_bucket.*.bucket}"]
+  value = ["${aws_s3_bucket.this.*.bucket}"]
   description = "Bucket name(s)"
 }
