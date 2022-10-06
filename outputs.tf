6c40bc2614bb4536527824d6c3dd71ab21fae131
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 22:36:49 -0500

    Add [*] to bucket refs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
-  value       = aws_s3_bucket.this.*.arn
+  value       = aws_s3_bucket.this[*].id
   description = "Bucket ARN(s)"
 }
 
 output "bucket_name" {
-  value       = aws_s3_bucket.this.*.arn
+  value       = aws_s3_bucket.this[*].id
   description = "Bucket name(s)"
 }
