f34da443dbf5007db701ca75e2f99adac442d1f8
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 20:24:16 -0500

    fix join name error

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
-  value       = join ("", aws_s3_bucket.this.*.arn)
+  value       = aws_s3_bucket.this.*.arn
   description = "Bucket ARN(s)"
 }
 
 output "bucket_name" {
-  value       = join ("", aws_s3_bucket.this.*.arn)
+  value       = aws_s3_bucket.this.*.arn
   description = "Bucket name(s)"
 }
