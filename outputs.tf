a4eaf43073b259fee14f1d3ab48cda5dc2717678
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 19:00:44 -0500

    Update indexing

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
-  value       = aws_s3_bucket.this[0].arn
+  value       = aws_s3_bucket.this[count.index].arn
   description = "Bucket ARN(s)"
 }
 
 output "bucket_name" {
-  value       = aws_s3_bucket.this[0].id
+  value       = aws_s3_bucket.this[count.index].id
   description = "Bucket name(s)"
 }
